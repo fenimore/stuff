733b9c9936dee7c239b38c367258257296a2bdee
Fenimore Love <exorable.ludos@gmail.com>
2020-11-11 02:00:22 -0500

    nvm

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.7',
 )
